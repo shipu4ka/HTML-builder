66a016624561231b38aa25793888223a02972f5d
Tatiana Shipulina <grafit.1@yandex.ru>
2022-11-01 10:44:17 +0300

    feat: add resolution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+let fs = require('fs');
+let stream = new fs.ReadStream(path.resolve('01-read-file', "text.txt"), {encoding: 'utf-8'});
+
+stream.on('readable', function () {
+    let data = stream.read();
+    if(data !== null) {
+    console.log(data);
+    }
+});
+
+
